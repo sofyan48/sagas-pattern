4a2da4c7e8d1fdc37a7813bbcc7077f4affc2528
sofyan48 <sofyan.saputra@ralali.com>
2020-04-11 03:10:32 +0700

    Refactor Consumer naming with controller

--- svc_payment/src/app/v1/consumer/controller/consumer.go
+++ svc_payment/src/app/v1/consumer/controller/consumer.go
@@ -1,103 +1,39 @@
-package consumer
+package controller
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
-	"os"
 	"time"
 
-	"github.com/Shopify/sarama"
-	"github.com/sofyan48/svc_payment/src/app/v1/entity"
-	"github.com/sofyan48/svc_payment/src/app/v1/event"
-	"github.com/sofyan48/svc_payment/src/utils/kafka"
-	"github.com/sofyan48/svc_payment/src/utils/logger"
+	"github.com/sofyan48/svc_order/src/app/v1/entity"
+	"github.com/sofyan48/svc_order/src/app/v1/event"
+	"github.com/sofyan48/svc_order/src/utils/kafka"
+	"github.com/sofyan48/svc_order/src/utils/logger"
 )
 
-// V1OrderEvents ...
-type V1OrderEvents struct {
+// ControllerEvent ....
+type ControllerEvent struct {
 	Kafka  kafka.KafkaLibraryInterface
-	Event  event.PaymentEventInterface
+	Event  event.UserEventInterface
 	Logger logger.LoggerInterface
 }
 
-// V1OrderEventsHandler ...
-func V1OrderEventsHandler() *V1OrderEvents {
-	return &V1OrderEvents{
+// ControllerEventHandler ...
+func ControllerEventHandler() *ControllerEvent {
+	return &ControllerEvent{
 		Kafka:  kafka.KafkaLibraryHandler(),
-		Event:  event.PaymentEventHandler(),
+		Event:  event.OrderEventHandler(),
 		Logger: logger.LoggerHandler(),
 	}
 }
 
-// V1OrderEventsInterface ...
-type V1OrderEventsInterface interface {
-	Consume(topics []string, signals chan os.Signal)
+// ControllerEventInterface ...
+type ControllerEventInterface interface {
+	PaymentSave(paymentData *entity.StateFullFormatKafka)
+	PaymentPaidOrder(paymentData *entity.StateFullFormatKafka)
 }
 
-// Consume ...
-func (consumer *V1OrderEvents) Consume(topics []string, signals chan os.Signal) {
-	// StateFullData := consumer.Kafka.GetStateFull()
-	chanMessage := make(chan *sarama.ConsumerMessage, 256)
-	csm, err := consumer.Kafka.InitConsumer()
-	if err != nil {
-		fmt.Println("Error: ", err)
-		panic(err)
-	}
-	for _, topic := range topics {
-		partitionList, err := csm.Partitions(topic)
-		if err != nil {
-			log.Println("Unable to get partition got error ", err)
-			continue
-		}
-		for _, partition := range partitionList {
-			go consumeMessage(csm, topic, partition, chanMessage)
-		}
-	}
-	log.Println("Event is Started....")
-
-ConsumerLoop:
-	for {
-		select {
-		case msg := <-chanMessage:
-			eventData := &entity.StateFullFormatKafka{}
-			json.Unmarshal(msg.Value, eventData)
-			switch eventData.Action {
-			case "payment_save":
-				consumer.paymentSave(eventData)
-			case "payment_order":
-				consumer.paymentPaidOrder(eventData)
-			default:
-				fmt.Println("OK")
-			}
-
-		case sig := <-signals:
-			if sig == os.Interrupt {
-				break ConsumerLoop
-			}
-		}
-	}
-}
-
-func consumeMessage(consumer sarama.Consumer, topic string, partition int32, c chan *sarama.ConsumerMessage) {
-	msg, err := consumer.ConsumePartition(topic, partition, sarama.OffsetNewest)
-	if err != nil {
-		log.Println("Unable to consume partition got error ", partition, err)
-		return
-	}
-	defer func() {
-		if err := msg.Close(); err != nil {
-			log.Println("Unable to close partition : ", partition, err)
-		}
-	}()
-	for {
-		msg := <-msg.Messages()
-		c <- msg
-	}
-
-}
-
-func (consumer *V1OrderEvents) paymentSave(paymentData *entity.StateFullFormatKafka) {
+// PaymentSave ...
+func (consumer *ControllerEvent) PaymentSave(paymentData *entity.StateFullFormatKafka) {
 	result, err := consumer.Event.InsertDatabase(paymentData)
 	if err != nil {
 		loggerData := map[string]interface{}{
@@ -141,7 +77,8 @@ func (consumer *V1OrderEvents) paymentSave(paymentData *entity.StateFullFormatKa
 
 }
 
-func (consumer *V1OrderEvents) paymentPaidOrder(paymentData *entity.StateFullFormatKafka) {
+// PaymentPaidOrder ...
+func (consumer *ControllerEvent) PaymentPaidOrder(paymentData *entity.StateFullFormatKafka) {
 	result, err := consumer.Event.PaymentUpdateOrder(paymentData)
 	if err != nil {
 		loggerData := map[string]interface{}{
